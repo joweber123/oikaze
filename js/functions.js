abda33baa01b257821d6a44fcaf3665ced9e4662
James Weber <joweber123@gmail.com>
2018-01-16 22:53:34 +0900

    added new section

--- js/functions.js
+++ js/functions.js
@@ -16,6 +16,7 @@ $(".close-button").click(function(){
 var container = document.querySelector('.instagram-feed');
 
 var cloneImg1 = document.querySelector('.insta-img-1').cloneNode(false);
+var cloneImg1c2 = document.querySelector('.insta-img-1').cloneNode(false);
 var cloneImg2c1 = document.querySelector('.insta-img-2').cloneNode(false);
 var cloneImg2c2 = document.querySelector('.insta-img-2').cloneNode(false);
 var cloneImg3c1 = document.querySelector('.insta-img-3').cloneNode(false);
@@ -24,20 +25,29 @@ var cloneImg4c1 = document.querySelector('.insta-img-4').cloneNode(false);
 var cloneImg4c2 = document.querySelector('.insta-img-4').cloneNode(false);
 var cloneImg5 = document.querySelector('.insta-img-5').cloneNode(false);
 
-container.insertBefore(cloneImg5, document.querySelector('.insta-img-1'));
-container.insertBefore(cloneImg4c1, document.querySelector('.insta-img-1'));
-container.insertBefore(cloneImg3c1, document.querySelector('.insta-img-1'));
+
+
+
+
+
 container.insertBefore(cloneImg2c1, document.querySelector('.insta-img-1'));
+container.insertBefore(cloneImg3c1, document.querySelector('.insta-img-1'));
+
+container.insertBefore(cloneImg4c1, document.querySelector('.insta-img-1'));
+
+container.insertBefore(cloneImg5, document.querySelector('.insta-img-1'));
 container.appendChild(cloneImg1);
 container.appendChild(cloneImg2c2);
 container.appendChild(cloneImg3c2);
 container.appendChild(cloneImg4c2);
 
 
-// Step 3: Adding an infinite scroll effect
+
+
+//Step 3: Adding an infinite scroll effect
 var sliderStartForward = document.querySelector('.insta-img:nth-child(5)').getBoundingClientRect().left;
-var sliderEndForward = document.querySelector('.insta-img:nth-child(9)').getBoundingClientRect().right - 10;
-var sliderStartBackward = document.querySelector('.insta-img:nth-child(5)').getBoundingClientRect().right;
+var sliderEndForward = document.querySelector('.insta-img:nth-child(9)').getBoundingClientRect().right - 9;
+var sliderStartBackward = document.querySelector('.insta-img:nth-child(6)').getBoundingClientRect().right;
 
 // We're repositionning our slider to our first true image 
 // as currently the first image we're seing is a clone
@@ -55,3 +65,4 @@ function scrolling() {
     container.scrollLeft = sliderStartForward;
   }
 }
+
